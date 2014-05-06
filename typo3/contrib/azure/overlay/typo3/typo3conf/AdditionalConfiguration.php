98a945d208012ffb48ea1de36697c7ddbd8adcbc
Johannes Goslar <johannes.goslar@dkd.de>
2014-05-06 09:35:24 +0200

    [AZURE] basic azure config

---
+++ typo3/contrib/azure/overlay/typo3/typo3conf/AdditionalConfiguration.php
@@ -0,0 +1,9 @@
+<?php
+//parameters will be set from parameters.xml
+$GLOBALS['TYPO3_CONF_VARS']['DB']['host'] = 'PlaceHolderForServer';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'PlaceHolderForDb';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'PlaceHolderForUser';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = 'PlaceHolderForPassword';
+//we need to disable the strict mode
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['setDBinit'] = "SET SESSION sql_mode='';";
+?>
