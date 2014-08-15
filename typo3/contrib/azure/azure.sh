16e8c7ae5bad09a8eaade34f59421f52141b02e4
Johannes Goslar <johannes.goslar@dkd.de>
2014-08-15 14:03:58 +0200

    [AZURE] auto feed

--- typo3/contrib/azure/azure.sh
+++ typo3/contrib/azure/azure.sh
@@ -3,3 +3,6 @@ cd ../../../../
 zip -r --filesync typo3/typo3/contrib/azure/package.zip typo3 -x "typo3/.git*" "typo3/typo3/contrib/azure/*"
 cd typo3/typo3/contrib/azure/overlay/
 zip -r --grow ../package.zip *
+cd ..
+SHA1=$(openssl sha1 package.zip | cut -d " " -f 2)
+sed -i '' "s/<sha1>.*<\/sha1>/<sha1>$SHA1<\/sha1>/g" feed.xml
