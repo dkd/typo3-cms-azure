f116e52903ecf99774d393a3dc634eef3d9b7c33
Johannes Goslar <johannes.goslar@dkd.de>
2015-02-24 18:57:06 +0000

    [AZURE] Initial Repacking

---
+++ azure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd typo3/
+git reset --hard HEAD
+git clean -x -f -d
+git apply ../patches/*
+cd ..
+zip -r --filesync package.zip typo3 install.sql manifest.xml parameters.xml web.config
+SHA1=$(openssl sha1 package.zip | cut -d " " -f 2)
+sed -i '' "s/<sha1>.*<\/sha1>/<sha1>$SHA1<\/sha1>/g" feed.xml
+echo $SHA1
