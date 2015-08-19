1987f8725220c90b53448793e32b2d9ebe604b29
dkd-azure <dkd-azure>
2015-08-19 12:20:07 +0200

    Windows Support in Buildscript

--- azure.sh
+++ azure.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+if [ $OSTYPE == "msys" ]
+then
+echo "On Windows"
+export PATH="/c/Program Files (x86)/IIS Express/PHP/v5.5/":$PATH
+export PATH="/c/Program Files (x86)/GnuWin32/bin":$PATH
+else
+echo "No Windows"
+fi
+git submodule update
 cd typo3/
 git reset --hard HEAD
-git clean -x -f -d
+git am --abort
+git clean -x -f -d --exclude="vendor"
 composer.phar install
-git apply -v ../patches/*
+git am ../patches/*
 cd ..
 zip -r --filesync package.zip typo3 install.sql manifest.xml parameters.xml web.config
 SHA1=$(openssl sha1 package.zip | cut -d " " -f 2)
-sed -i '' "s/<sha1>.*<\/sha1>/<sha1>$SHA1<\/sha1>/g" feed.xml
+sed -i'' "s|<sha1>.*</sha1>|<sha1>$SHA1</sha1>|g" feed.xml
 echo $SHA1
+if [ $OSTYPE == "msys" ]
+then
+sed -i'' "s|<installerURL>.*</installerURL>|<installerURL>C:\\\\Users\\\\dkd-azure\\\\typo3\\\\package.zip</installerURL>|g" feed.xml
+else
+sed -i'' "s|<installerURL>.*</installerURL>|<installerURL>file://\\\\\\\\VBOXSVR\\\\azure\\\\package.zip</installerURL>|g" feed.xml
+fi
