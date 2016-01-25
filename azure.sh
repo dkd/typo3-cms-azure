0c8eaa748003b15c8f32fc7dcdadf5b490baeebe
Johannes Goslar <johannes.goslar@dkd.de>
2016-01-25 15:10:10 +0100

    TYPO3 8

--- azure.sh
+++ azure.sh
@@ -7,21 +7,22 @@ export PATH="/c/Program Files (x86)/GnuWin32/bin":$PATH
 else
 echo "No Windows"
 fi
-git submodule update
 cd typo3/
+git checkout origin/master
 git reset --hard HEAD
 git am --abort
 git clean -x -f -d --exclude="vendor"
 composer.phar install
-git am ../patches/*
+git am -k ../patches/*
 cd ..
 zip -r --filesync package.zip typo3 install.sql manifest.xml parameters.xml web.config
 SHA1=$(openssl sha1 package.zip | cut -d " " -f 2)
-sed -i'' "s|<sha1>.*</sha1>|<sha1>$SHA1</sha1>|g" feed.xml
 echo $SHA1
 if [ $OSTYPE == "msys" ]
 then
-sed -i'' "s|<installerURL>.*</installerURL>|<installerURL>C:\\\\Users\\\\dkd-azure\\\\typo3\\\\package.zip</installerURL>|g" feed.xml
+    sed -i'' "s|<sha1>.*</sha1>|<sha1>$SHA1</sha1>|g" feed.xml
+    sed -i'' "s|<installerURL>.*</installerURL>|<installerURL>C:\\\\Users\\\\dkd-azure\\\\typo3\\\\package.zip</installerURL>|g" feed.xml
 else
-sed -i'' "s|<installerURL>.*</installerURL>|<installerURL>file://\\\\\\\\VBOXSVR\\\\azure\\\\package.zip</installerURL>|g" feed.xml
+    sed -i '' -e "s|<sha1>.*</sha1>|<sha1>$SHA1</sha1>|g" feed.xml
+    sed -i '' -e "s|<installerURL>.*</installerURL>|<installerURL>file://\\\\\\\\VBOXSVR\\\\azure\\\\package.zip</installerURL>|g" feed.xml
 fi
